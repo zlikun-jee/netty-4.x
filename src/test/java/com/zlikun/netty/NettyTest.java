67f3e1c45d1e3949f12035f914cb3647cbfd9d1a
zlikun <zlikun.dev@hotmail.com>
2021-02-27 21:03:27 +0800

    Init .

---
+++ src/test/java/com/zlikun/netty/NettyTest.java
@@ -0,0 +1,17 @@
+package com.zlikun.netty;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://junit.org/junit5/docs/current/user-guide/
+ */
+@Slf4j
+public class NettyTest {
+
+    @Test
+    void test() {
+        log.info("Hello Netty!");
+    }
+
+}
